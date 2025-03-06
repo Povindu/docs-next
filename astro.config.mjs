3b37045a0103708330add427148dddb5f8bff481
Povindu Samarasekara <povinduchanmith@gmail.com>
2025-03-06 09:15:54 +0530

    refactor: astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,25 +1,25 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
-// import remarkDirective from "remark-directive";
-// import { remarkDirectiveNote } from "./remark-directive-note.mjs";
-// import codeImport from "remark-code-import";
+import remarkDirective from "remark-directive";
+import { remarkDirectiveNote } from "./remark-directive-note.mjs";
+import codeImport from "remark-code-import";
 import mdx from "@astrojs/mdx";
 import preact from "@astrojs/preact";
 import robots from "astro-robots";
 import sitemap from "@astrojs/sitemap";
 import pagefind from "astro-pagefind";
-// import { rehypeHeadingIds } from '@astrojs/markdown-remark';
-//import rehypeSlug from 'rehype-slug';
-// import rehypeAutolinkHeadings from 'rehype-autolink-headings';
-// import rehypeSanitize from 'rehype-sanitize';
+import { rehypeHeadingIds } from "@astrojs/markdown-remark";
+import rehypeSlug from "rehype-slug";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
+import rehypeSanitize from "rehype-sanitize";
 
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   redirects: {
-    // '/release-notes': '/release-notes/index',
+    //  Sample way to create redirects
     // '/blog': 'https://example.com/blog'
   },
   integrations: [
@@ -46,10 +46,10 @@ export default defineConfig({
       },
     }),
     mdx(),
-    // preact(),
     robots(),
     sitemap(),
     pagefind(),
+    // preact(),
     react(),
   ],
   build: {
