8f09a61a8122d53c2da8d6e2d464353c444560d0
Povindu Samarasekara <povinduchanmith@gmail.com>
2025-05-06 12:08:02 +0530

    feat: added nodejs adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,16 +5,15 @@ import remarkDirective from "remark-directive";
 import { remarkDirectiveNote } from "./remark-directive-note.mjs";
 import codeImport from "remark-code-import";
 import mdx from "@astrojs/mdx";
-import preact from "@astrojs/preact";
 import robots from "astro-robots";
 import sitemap from "@astrojs/sitemap";
 import pagefind from "astro-pagefind";
 import { rehypeHeadingIds } from "@astrojs/markdown-remark";
 //import rehypeSlug from 'rehype-slug';
 import rehypeAutolinkHeadings from "rehype-autolink-headings";
-import rehypeSanitize from "rehype-sanitize";
-
+// import rehypeSanitize from "rehype-sanitize";
 import react from "@astrojs/react";
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
@@ -31,6 +30,7 @@ export default defineConfig({
     ],
     // rehypeSanitize, rehypeSlug
   },
+
   integrations: [
     tailwind({
       applyBaseStyles: false,
@@ -55,15 +55,16 @@ export default defineConfig({
       },
     }),
     mdx(),
-    // preact(),
     robots(),
     sitemap(),
     pagefind(),
     react(),
   ],
+
   image: {
     service: sharpImageService({ limitInputPixels: false }),
   },
+
   i18n: {
     locales: ["da", "de", "en", "nl", "no", "sv"],
     defaultLocale: "en",
@@ -75,10 +76,16 @@ export default defineConfig({
       de: "en",
     },
   },
+
   build: {
     format: "preserve",
   },
+
   site: "https://superofficedocs.github.io",
   base: "/docs-next",
   trailingSlash: "never",
+  adapter: node({
+    mode: "standalone",
+    output: 'static',
+  }),
 });
